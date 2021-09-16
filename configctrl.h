1387e3e6f180f5ee0b6ce948ee8d95fc88a14e63
HiraethOni <HiraethOni@outlook.com>
2021-09-17 07:08:28 +0800

    Docs: 添加注释

--- configctrl.h
+++ configctrl.h
@@ -9,14 +9,40 @@ class configCtrl
 public:
     configCtrl();
 
+    /**
+     * 配置文件写入函数
+     * @param group 组名
+     * @param key key
+     * @param value 值
+     * @return 写入状态
+     */
     bool writeInit(QString group, QString key, QString value);
+    /**
+     * 配置文件读取函数
+     * @param group 组名
+     * @param key key
+     * @param value 读取到的值的存放指针
+     * @return 读取状态
+     */
     bool readInit(QString group, QString key, QString &value);
+    /**
+     * 配置文件读取函数-直接返回读取结果
+     * @param group 组名
+     * @param key key
+     * @return 读取值
+     */
     QString readInit(QString group, QString key);
 
 private:
-    QString initFilePath = "./config.ini";
+    QString initFilePath = "./config.ini";      // 配置文件所处路径
+    /**
+     * 检查程序运行目录内是否有配置文件
+     * 没有则新建
+     * @return
+     */
     bool createConfigFile();
 
+    // 默认配置文件,注意路径需要加\转义
     QString defaultConcig = R"([mariadb]
 host=localhost
 username=username
@@ -28,10 +54,21 @@ language=zh_CN
 path=X:\\ProgramData\\Cadence\\Cadence_Lib\\Library\\Package)";
 };
 
+/**
+ * 专门读取数据库配置文件的派生类
+ */
 class configDatabase : private configCtrl
 {
 public:
+    /**
+     * 读取数据库配置文件
+     * @return QStringList
+     */
     QStringList readDatabase();
+    /**
+     * 写入数据库配置文件
+     * @param str_list 待写入的列表
+     */
     void writeDatabase(QStringList &str_list);
 private:
     QStringList db_conf_key_list = {"host", "username", "passwd", "datasheet"};
