da2797311140f538391427cc46cbf387bea3d5c2
HiraethOni <HiraethOni@outlook.com>
2021-09-18 10:41:49 +0800

    Stash: 弹出之前的储存

--- databasectrl.cpp
+++ databasectrl.cpp
@@ -5,6 +5,9 @@
 QSqlDatabase databaseCtrl::db = QSqlDatabase::addDatabase("QODBC");
 databaseCtrl::databaseCtrl(QObject *parent) : QObject(parent)
 {
+    table_column_name<<"ID"<<"Part Number"<<"Description"<<"Part Type"<<"Value"<<"MFG Name"<<"PCB Footprint"<<"Symbol"<<"MFG Part Name"
+        <<"MFG Part Description"<<"RoHS"<<"MFG Part Lifecycle Phase"<<"Datasheet"<<"buy link";
+    p_query = new QSqlQuery(db);
     this->connectDB();
 }
 
@@ -28,4 +31,18 @@ bool databaseCtrl::connectDB() {
 
 QStringList databaseCtrl::getAllTablesNames() const {
     return this->db.tables();
+}
+
+QString databaseCtrl::getAllContent(const QString tables_name) const {
+    p_query->exec("Select * from "+tables_name);
+    QStringList str_list;
+    while(p_query->next()){
+        QStringList temp_list;
+        temp_list << "\"" + table_column_name[0] + "\":\"" + QString::number(p_query->value(0).toInt()) + "\"";
+        for (int i = 1; i < table_column_name.count(); i++){
+            temp_list << "\"" + table_column_name[i] + "\":\"" + p_query->value(i).toString() + "\"";
+        }
+        str_list<<"{" + temp_list.join(",") + "}";
+    }
+    return "[" + str_list.join(",") + "]";
 }
--- databasectrl.h
+++ databasectrl.h
@@ -1,6 +1,7 @@
 #ifndef DATABASECTRL_H
 #define DATABASECTRL_H
 #include <QSqlDatabase>
+#include <QSqlQuery>
 #include <QObject>
 
 class databaseCtrl : public QObject
@@ -9,9 +10,12 @@ class databaseCtrl : public QObject
 public:
     explicit databaseCtrl(QObject *parent=nullptr);
     Q_INVOKABLE QStringList getAllTablesNames() const;
+    Q_INVOKABLE QString getAllContent(const QString tables_name) const;
     static QSqlDatabase db;
 private:
     bool connectDB();
+    QSqlQuery *p_query;
+    QStringList table_column_name;
 };
 
 #endif // DATABASECTRL_H
