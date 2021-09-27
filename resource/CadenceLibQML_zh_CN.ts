88f942e864fd94a6d004f1df303997d0e2464d0b
HiraethOni <HiraethOni@outlook.com>
2021-09-27 12:21:03 +0800

    Feat: CadenceLibQML_zh_CN.ts 翻译

--- resource/CadenceLibQML_zh_CN.ts
+++ resource/CadenceLibQML_zh_CN.ts
@@ -1,12 +1,57 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="CadenceLibQML_zh_CN">
+<TS version="2.1" language="zh_CN">
 <context>
     <name>main</name>
     <message>
-        <location filename="main.qml" line="6"/>
+        <location filename="main.qml" line="9"/>
         <source>CadenceLib</source>
+        <translation type="unfinished">OrCAD 数据库管理器</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="42"/>
+        <source>Add</source>
+        <translation type="unfinished">新增</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="61"/>
+        <source>Part Number</source>
         <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="66"/>
+        <source>Description</source>
+        <translation type="unfinished">详情</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="92"/>
+        <source>Value</source>
+        <translation type="unfinished">值</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="97"/>
+        <source>MFG Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="134"/>
+        <source>MFG Part Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="139"/>
+        <source>MFG Part Description</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="154"/>
+        <source>Datasheet</source>
+        <translation type="unfinished">数据手册</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="159"/>
+        <source>Buy link</source>
+        <translation type="unfinished">购买链接</translation>
     </message>
 </context>
 </TS>
